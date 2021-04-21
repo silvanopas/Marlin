0d605e28ba6b1950583072395aa7e34a1ee54675
Mike La Spina <mike.laspina@shaw.ca>
2021-04-20 21:32:19 -0500

    Bed set to 580 x 480

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1226,8 +1226,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 580
+#define Y_BED_SIZE 480
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1235,7 +1235,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 180
 
 /**
  * Software Endstops
