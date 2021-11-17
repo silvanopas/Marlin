e1e9907ce6506267aeafb019bc1c4b544ecd1522
Mike La Spina <mike.laspina@shaw.ca>
2021-11-17 16:39:53 -0600

    set pwm pin map

--- Marlin/src/HAL/STM32/fast_pwm.cpp
+++ Marlin/src/HAL/STM32/fast_pwm.cpp
@@ -34,38 +34,57 @@ void set_pwm_duty(const pin_t pin, const uint16_t v, const uint16_t v_size/*=255
   TIM_TypeDef *Instance = (TIM_TypeDef *)pinmap_peripheral(pin_name, PinMap_PWM);
   HardwareTimer *HT;
   TimerModes_t previousMode;
+
   uint32_t index = get_timer_index(Instance);
   if (HardwareTimer_Handle[index] == NULL) {
     HardwareTimer_Handle[index]->__this = new HardwareTimer((TIM_TypeDef *)pinmap_peripheral(pin_name, PinMap_PWM));
     needs_freq = true;                  // The instance must be new set the default frequency of PWM_FREQUENCY
   }
+
   HT = (HardwareTimer *)(HardwareTimer_Handle[index]->__this);
   uint32_t channel = STM_PIN_CHANNEL(pinmap_function(pin_name, PinMap_PWM));
-  uint16_t adj_val = v;
-  if (invert) adj_val = v_size - v;
-
   previousMode = HT->getMode(channel);
+
   if (previousMode != TIMER_OUTPUT_COMPARE_PWM1) {
     HT->setMode(channel, TIMER_OUTPUT_COMPARE_PWM1, pin);
   }
+
   if (needs_freq) HT->setOverflow(PWM_FREQUENCY, HERTZ_FORMAT);
-  HT->setCaptureCompare(channel, adj_val, RESOLUTION_8B_COMPARE_FORMAT);
+
+  uint16_t value = LL_TIM_GetAutoReload(Instance) * v / v_size;
+  if (invert) value = v_size - value;
+
+  HT->setCaptureCompare(channel, value, RESOLUTION_8B_COMPARE_FORMAT);
   if (previousMode != TIMER_OUTPUT_COMPARE_PWM1) {
     HT->resume();
   }
+  pinmap_pinout(pin_name, PinMap_PWM);
+
+  SERIAL_ECHO_MSG("GetCounter",LL_TIM_GetCounter(Instance));
+  SERIAL_ECHO_MSG("GetPrescaler",LL_TIM_GetPrescaler(Instance));
+  SERIAL_ECHO_MSG("GetAutoReload",LL_TIM_GetAutoReload(Instance));
+  SERIAL_ECHO_MSG("IsEnabledCounter",LL_TIM_IsEnabledCounter(Instance));
+  SERIAL_ECHO_MSG("GetCounterMode",LL_TIM_GetCounterMode(Instance));
+  SERIAL_ECHO_MSG("IsEnabledARRPreload",LL_TIM_IsEnabledARRPreload(Instance));  
+  SERIAL_ECHO_MSG("GetCompareCH1",LL_TIM_OC_GetCompareCH1(Instance));
+  SERIAL_ECHO_MSG("GetCompareCH2",LL_TIM_OC_GetCompareCH2(Instance));
+  SERIAL_ECHO_MSG("GetCompareCH3",LL_TIM_OC_GetCompareCH3(Instance));
+  SERIAL_ECHO_MSG("GetCompareCH4",LL_TIM_OC_GetCompareCH4(Instance));
 }
 
 void set_pwm_frequency(const pin_t pin, int f_desired) {
   if (!PWM_PIN(pin)) return;            // Don't proceed if no hardware timer
-
+  HardwareTimer *HT;
   PinName pin_name = digitalPinToPinName(pin);
   TIM_TypeDef *Instance = (TIM_TypeDef *)pinmap_peripheral(pin_name, PinMap_PWM); // Get HAL timer instance
+  
+  uint32_t index = get_timer_index(Instance);
 
-  LOOP_S_L_N(i, 0, NUM_HARDWARE_TIMERS) // Protect used timers
-    if (timer_instance[i] && timer_instance[i]->getHandle()->Instance == Instance)
-      return;
+  if (HardwareTimer_Handle[index] != NULL) return;
 
-  pwm_start(pin_name, f_desired, 0, RESOLUTION_8B_COMPARE_FORMAT);
+  HardwareTimer_Handle[index]->__this = new HardwareTimer((TIM_TypeDef *)pinmap_peripheral(pin_name, PinMap_PWM));
+  HT = (HardwareTimer *)(HardwareTimer_Handle[index]->__this);
+  HT->setOverflow(f_desired, HERTZ_FORMAT);
 }
 
 #endif // HAL_STM32
