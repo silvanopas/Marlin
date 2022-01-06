63dbdf9a566931dd2352453171d92e6e8ee72a47
descipher <mike.laspina@shaw.ca>
2022-01-06 00:50:06 -0600

    No set_pwm_frequency exception

--- Marlin/src/module/stepper.cpp
+++ Marlin/src/module/stepper.cpp
@@ -3262,7 +3262,11 @@ void Stepper::report_positions() {
       #elif HAS_MOTOR_CURRENT_PWM
 
         #define _WRITE_CURRENT_PWM(P) set_pwm_duty(pin_t(MOTOR_CURRENT_PWM_## P ##_PIN), 255L * current / (MOTOR_CURRENT_PWM_RANGE))
-        #define _WRITE_FREQUENCY_PWM(P) set_pwm_frequency(pin_t(MOTOR_CURRENT_PWM_## P ##_PIN), MOTOR_CURRENT_PWM_FREQUENCY)
+        #ifdef __AVR__
+          #define _WRITE_FREQUENCY_PWM(P) set_pwm_frequency(pin_t(MOTOR_CURRENT_PWM_## P ##_PIN), MOTOR_CURRENT_PWM_FREQUENCY)
+        #else
+          #define _WRITE_FREQUENCY_PWM(P) NOOP()  
+        #endif  
         switch (driver) {
           case 0:
             #if PIN_EXISTS(MOTOR_CURRENT_PWM_X)
